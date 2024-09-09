d3e0e794a61c1c00de16e5dfd258129d62e94481
iecube-zhangxiaolong <xiaolong.zhang@iecube.com.cn>
2024-09-09 19:05:25 +0800

    drag meter

--- src/views/meter/measurementslive.vue
+++ src/views/meter/measurementslive.vue
@@ -1,17 +1,12 @@
 <template>
     <iframe ref="measurementslive" id="measurementslive_iframe" src="https://measurementslive.ni.com/measure.html"
         frameborder="0"></iframe>
-    <el-button @click="jietu()">截图</el-button>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import ScreenShot from "js-web-screen-shot"
 
 const measurementslive = ref()
-const jietu = () => {
-    new ScreenShot({ enableWebRtc: false })
-}
 </script>
 <style scoped>
 iframe {
