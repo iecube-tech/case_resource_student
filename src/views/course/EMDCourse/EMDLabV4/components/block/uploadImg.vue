33afaa051cb59113719a3959cc465e3d2dcd5b9f
iecube-zhuxiaoxi <xiaoxi.zhu@iecube.com.cn>
2025-10-23 18:51:57 +0800

    上传图片增加放大功能

--- src/views/course/EMDCourse/EMDLabV4/components/block/uploadImg.vue
+++ src/views/course/EMDCourse/EMDLabV4/components/block/uploadImg.vue
@@ -5,11 +5,7 @@
 
     <!-- Upload Section -->
     <div>
-      <el-upload
-        :show-file-list="false"
-        :before-upload="beforeUpload"
-        :disabled="blockStatusDisabled"
-      >
+      <el-upload :show-file-list="false" :before-upload="beforeUpload" :disabled="blockStatusDisabled">
         <template #trigger>
           <el-button type="primary" :disabled="blockStatusDisabled">
             <font-awesome-icon icon="fa-solid fa-cloud-arrow-up" class="mr-2"></font-awesome-icon>
@@ -20,17 +16,21 @@
 
       <!-- Preview Uploaded Image -->
       <div v-if="comp.payload.upload.imgUrl" class="mt-4 flex justify-center">
-        <img :src="`/dev-api${comp.payload.upload.imgUrl}`" alt="Uploaded Image" class="h-[400px] w-auto rounded" />
+        <img :src="`/dev-api${comp.payload.upload.imgUrl}`" alt="Uploaded Image"
+          class="h-[400px] w-auto rounded md-image" @click.native="handleClick" />
       </div>
     </div>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import textpreview from '@/components/textPreview.vue';
 import { useEmdV4Store } from '@/stores/emdV4TaskStore';
 import { uploadImg } from '@/apis/emdV4/index';
 import { updateCompPayload, updateCompStatus } from './update';
+
+import { useEmdStore } from '@/stores/emdLabStore';
+const labStore = useEmdStore()
 
 // Props
 const props = defineProps({
@@ -57,17 +57,17 @@ const beforeUpload = (file) => {
   const allowedTypes = ['image/jpeg', 'image/png'];
   if (!allowedTypes.includes(file.type)) {
     ElMessage.warning('仅支持上传 JPG、PNG 格式的图片。');
-    f= false; // Abort upload
+    f = false; // Abort upload
   }
 
   // Maximum file size (e.g., 5MB)
   const maxSize = 2 * 1024 * 1024; // 5MB in bytes
   if (file.size > maxSize) {
     ElMessage.warning('文件大小不能超过 2MB。');
-    f= false; // Abort upload
+    f = false; // Abort upload
   }
-  
-  if(f){
+
+  if (f) {
     autoUpload(file)
   }
 
@@ -91,10 +91,10 @@ const autoUpload = async (file) => {
 
       // Update the payload
       const payloadStr = JSON.stringify(props.comp.payload);
-      updateCompPayload( props.comp.id, payloadStr);
+      updateCompPayload(props.comp.id, payloadStr);
 
       // Update the component status
-      if(props.comp.status === 0){
+      if (props.comp.status === 0) {
         updateCompStatus(props.comp.id, 1);
       }
     } else {
@@ -105,6 +105,15 @@ const autoUpload = async (file) => {
     ElMessage.error('图片上传失败，请稍后重试。');
   }
 };
+
+const handleClick = (event) => {
+  if (props.comp.payload.upload.imgUrl != '') {
+    if ((event.target as HTMLElement).classList.contains("md-image")) {
+      labStore.showZoomed = true
+      labStore.setimageSrc((event.target as HTMLImageElement).currentSrc)
+    }
+  }
+}
 </script>
 
 <style scoped>
